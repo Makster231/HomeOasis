185febb50f1b5f3bd934b4cf515822dc90582455
38071 <makster559553@gmail.com>
2020-05-06 00:17:32 +0300

    added start section

--- app/js/component/custom.js
+++ app/js/component/custom.js
@@ -1,6 +1,6 @@
 (function ($, window, document) {
   $(() => {
+    //show content after loaded page
     $("body").css("opacity", 1);
-    $(".nav-list").addClass("nav-animation-show");
   });
 })(window.jQuery, window, document);
