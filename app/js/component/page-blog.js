eb34db85a3df9f06fdd7857765202db288ff779b
38071 <makster559553@gmail.com>
2020-05-07 01:04:55 +0300

    activate blog page slider

--- app/js/component/page-blog.js
+++ app/js/component/page-blog.js
@@ -13,9 +13,29 @@
     // добавляем стрелкам метку для слайдера
     $(".js_footer-navigation").addClass("js_navigation-blog--page");
   }
+
+  function blogPageSlider() {
+    $(".js_page-blog--slider").owlCarousel({
+      items: 1,
+      loop: true,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 10000,
+      smartSpeed: 1000,
+      nav: true,
+      navText: [
+        "<span class='icon-owl icon-prev'></span>",
+        "<span class='icon-owl icon-next'></span>",
+      ],
+    });
+  }
+
   $(() => {
     if ($(".js_blog-page").length) {
       checkIsItBlogPage();
+      blogPageSlider();
+      // bind arrows for blog slider 
+      SliderNavigation(true, ".js_page-blog--slider");
     }
   });
 })(window.jQuery, window, document);
