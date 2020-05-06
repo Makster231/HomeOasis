defffed49e57b85684690100960912ddb3b145ba
38071 <makster559553@gmail.com>
2020-05-06 04:11:42 +0300

    changed cursor / added click on logo

--- app/js/component/navigation.js
+++ app/js/component/navigation.js
@@ -1,5 +1,5 @@
 (function ($, window, document) {
-  function menuStartSection() {
+  function navigation() {
     $(".js_nav-item").click(function () {
       let $this = $(this);
       // Показываем вВерхнее меню
@@ -10,20 +10,37 @@
       $(".js_nav-item").removeClass("js_active");
 
       if ($this.is(".js_nav-portfolio")) {
-        $(".js_section-portfolio").removeClass("js_hidden").addClass("js_animation-portfolio");
+        $(".js_section-portfolio")
+          .removeClass("js_hidden")
+          .addClass("js_animation-portfolio");
         $(".js_footer-navigation, .js_nav-portfolio").addClass("js_active");
         $(".js_footer-description").addClass("js_hidden");
       }
 
       if ($this.is(".js_nav-video")) {
-        $(".js_section-video").removeClass("js_hidden").addClass("js_animation-video");
+        $(".js_section-video")
+          .removeClass("js_hidden")
+          .addClass("js_animation-video");
         $(".js_nav-video").addClass("js_active");
-
       }
     });
   }
 
+  function logoNavigation() {
+    $(".js_logo").click(function () {
+      // Скрываем вВерхнее меню
+      $(".js_header-menu").removeClass("js_active");
+      // Скрываем текущий экран
+      $(".js_section").addClass("js_hidden");
+      // ПОказываем начальный экран
+      $(".js_section-start").removeClass("js_hidden");
+      // Убираем js_active со всех пунктов меню
+      $(".js_nav-item").removeClass("js_active");
+    });
+  }
+
   $(() => {
-    menuStartSection();
+    navigation();
+    logoNavigation();
   });
 })(window.jQuery, window, document);
