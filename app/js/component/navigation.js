a79ba34c46c2a3a0c9eb1c505a144c2aa4dc0b77
38071 <makster559553@gmail.com>
2020-05-06 05:46:57 +0300

    added blog section

--- app/js/component/navigation.js
+++ app/js/component/navigation.js
@@ -25,6 +25,17 @@
         $(".js_nav-video").addClass("js_active");
       }
 
+      if ($this.is(".js_nav-blog")) {
+        // Показываем блок Блог
+        $(".js_section-blog").removeClass("js_hidden");
+        // Показываем стрелки, добавляем класс пункту меню
+        $(".js_footer-navigation, .js_nav-blog").addClass("js_active");
+        // убираем текст в футере
+        $(".js_footer-description").addClass("js_hidden");
+        // добавляем стрелкам метку для слайдера
+        $(".js_footer-navigation").addClass("js_navigation-blog");
+      }
+
       if ($this.is(".js_nav-contacts")) {
         // Меняем цвет фона на белый
         $("body").addClass("bg-color--white");
