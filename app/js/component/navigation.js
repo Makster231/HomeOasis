9d8a7952d7ad776aa5f9e50d615421b38b64e509
38071 <makster559553@gmail.com>
2020-05-06 04:02:45 +0300

    added animation to sections "start / portfolio / video"

--- app/js/component/navigation.js
+++ app/js/component/navigation.js
@@ -2,20 +2,23 @@
   function menuStartSection() {
     $(".js_nav-item").click(function () {
       let $this = $(this);
-      // Скрываем загрузочынй экран
-      $(".js_section-start").addClass("js_hidden");
       // Показываем вВерхнее меню
       $(".js_header-menu").addClass("js_active");
+      // Скрываем текущий экран
+      $(".js_section").addClass("js_hidden");
+      // Убираем js_active со всех пунктов меню
+      $(".js_nav-item").removeClass("js_active");
 
       if ($this.is(".js_nav-portfolio")) {
-        $(".js_section-portfolio").removeClass("js_hidden");
+        $(".js_section-portfolio").removeClass("js_hidden").addClass("js_animation-portfolio");
         $(".js_footer-navigation, .js_nav-portfolio").addClass("js_active");
         $(".js_footer-description").addClass("js_hidden");
       }
 
       if ($this.is(".js_nav-video")) {
-        $(".js_section-video").removeClass("js_hidden");
+        $(".js_section-video").removeClass("js_hidden").addClass("js_animation-video");
         $(".js_nav-video").addClass("js_active");
+
       }
     });
   }
--- app/js/component/section-video.js
+++ app/js/component/section-video.js
@@ -1,10 +1,7 @@
 (function ($, window, document) {
-  function playOnHover() {
-  }
+  function playOnHover() {}
 
   $(() => {
-    playOnHover();
-
     // Activate Plyr Player
     const players = Array.from(document.querySelectorAll(".js_player")).map(
       (p) => new Plyr(p)
