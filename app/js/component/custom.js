abeec7a0e9a6742ef2505f7a77c4fd7c98a2fdf4
38071 <makster559553@gmail.com>
2020-05-07 23:56:19 +0300

    added logo animation

--- app/js/component/custom.js
+++ app/js/component/custom.js
@@ -1,6 +1,33 @@
 (function ($, window, document) {
+  async function logoAnimation() {
+    let $logoLetters_length = $(".js_logo .cls-1").length;
+
+    for (let i = 0; i < $logoLetters_length; i++) {
+      $(
+        ".js_logo .cls-1.js_logo-animation:eq(" +
+          i +
+          "), .js_logo .cls-2.js_logo-animation:eq(" +
+          i +
+          ")"
+      ).addClass("logo_letter_animation");
+      await delay(400);
+      $(
+        ".js_logo .cls-1.js_logo-animation:eq(" +
+          i +
+          ") , .js_logo .cls-2.js_logo-animation:eq(" +
+          i +
+          ")"
+      ).removeClass("logo_letter_animation");
+
+      if ($logoLetters_length - 1 === i) {
+        logoAnimation();
+      }
+    }
+  }
+
   $(() => {
     //show content after loaded page
     $("body").css("opacity", 1);
+    logoAnimation();
   });
 })(window.jQuery, window, document);
