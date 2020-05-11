8bde647989f75ab1b04a934d7e34f9fbf9864487
38071 <makster559553@gmail.com>
2020-05-12 01:28:45 +0300

    remove audio from video on hover / add full screen player video on click

--- app/js/component/section-video.js
+++ app/js/component/section-video.js
@@ -1,30 +1,52 @@
 (function ($, window, document) {
+  // Выход из полноэкранного режима
+  function cancelFullscreen() {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+
   // Play video on hover
   function playOnHover() {
     $(".js_section-video--block").on("mouseover", function () {
-      $(this).find("video").get(0).play();
+      var $this_video = $(this).find("video").get(0);
+      $this_video.play();
     });
     $(".js_section-video--block").on("mouseleave", function () {
-      $(this).find("video").get(0).pause();
+      var $this_video = $(this).find("video").get(0);
+      $this_video.muted = true;
+      $this_video.pause();
     });
-  }
-
-  function videoPlayerInit() {
-    let players = document.querySelectorAll(".js-player"),
-      counter = 0;
-
-    if (!players.length) {
-      return;
-    }
-
-    for (let i = 0; i < players.length; i++) {
-      new Plyr(players[i]);
-    }
+    $(".js_section-video--block").on("click", function () {
+      var $this_video = $(this).find("video").get(0);
+      $this_video.muted = false;
+      if ($this_video.requestFullscreen) {
+        $this_video.requestFullscreen();
+      } else if ($this_video.mozRequestFullScreen) {
+        /* Firefox */
+        $this_video.mozRequestFullScreen();
+      } else if ($this_video.webkitRequestFullscreen) {
+        /* Chrome, Safari & Opera */
+        $this_video.webkitRequestFullscreen();
+      } else if ($this_video.msRequestFullscreen) {
+        /* IE/Edge */
+        $this_video.msRequestFullscreen();
+      }
+    });
+    $(".js_section-video--block").on("dblclick", function () {
+      cancelFullscreen();
+    });
   }
 
   $(() => {
     // Activate Plyr Player
-    videoPlayerInit();
+    const players = Array.from(document.querySelectorAll(".js-player")).map(
+      (p) => new Plyr(p)
+    );
     // Play video on hover
     playOnHover();
   });
